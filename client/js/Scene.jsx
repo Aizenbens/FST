c2016052bcb6e8338015ef379d2d4fad1a9343c4
Aizenbens <sheninozen@gmail.com>
2026-07-25 13:01:36 +0100

    Update Scene.jsx

--- client/js/Scene.jsx
+++ client/js/Scene.jsx
@@ -1,31 +1,30 @@
 import { Canvas } from "@react-three/fiber";
-import { PointerLockControls } from "@react-three/drei";
-
-import Player from "./Player";
-import World from "./World";
 
 export default function Scene() {
-  return (
-    <Canvas
-      shadows
-      camera={{
-        position: [0, 2, 6],
-        fov: 75
-      }}
-    >
-      <PointerLockControls />
+    return (
+        <Canvas
+            camera={{
+                position: [0, 2, 5],
+                fov: 75
+            }}
+        >
+            <ambientLight intensity={2} />
 
-      <ambientLight intensity={1.5} />
+            <directionalLight
+                position={[5, 10, 5]}
+                intensity={2}
+            />
 
-      <directionalLight
-        position={[5, 10, 5]}
-        intensity={2}
-        castShadow
-      />
+            <mesh rotation={[-Math.PI / 2, 0, 0]}>
+                <planeGeometry args={[200, 200]} />
+                <meshStandardMaterial color="#4caf50" />
+            </mesh>
 
-      <World />
+            <mesh position={[0, 1, 0]}>
+                <boxGeometry />
+                <meshStandardMaterial color="orange" />
+            </mesh>
 
-      <Player />
-    </Canvas>
-  );
+        </Canvas>
+    );
 }
