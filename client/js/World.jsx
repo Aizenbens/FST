0240609032274ed9eec39f795d5dd5069c7b68b4
Aizenbens <sheninozen@gmail.com>
2026-07-25 12:44:51 +0100

    Update World.jsx

--- client/js/World.jsx
+++ client/js/World.jsx
@@ -1,1 +1,11 @@
-
+export default function World() {
+    return (
+        <mesh
+            rotation={[-Math.PI / 2, 0, 0]}
+            receiveShadow
+        >
+            <planeGeometry args={[100, 100]} />
+            <meshStandardMaterial color="#49b14d" />
+        </mesh>
+    );
+}
