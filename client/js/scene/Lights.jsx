cf4a9bcb75f65769836c05f4a89f8a1dadba6e90
Aizenbens <sheninozen@gmail.com>
2026-07-25 13:23:29 +0100

    Update Lights.jsx

--- client/js/scene/Lights.jsx
+++ client/js/scene/Lights.jsx
@@ -1,1 +1,17 @@
+import React from "react";
 
+export default function Lights() {
+    return (
+        <>
+            <ambientLight intensity={1.5} />
+
+            <directionalLight
+                castShadow
+                intensity={2}
+                position={[15, 20, 10]}
+                shadow-mapSize-width={2048}
+                shadow-mapSize-height={2048}
+            />
+        </>
+    );
+}
