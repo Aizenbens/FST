11588c0a53f9c4ca6c943dde8b0d1ab31152db9c
Aizenbens <sheninozen@gmail.com>
2026-07-25 17:42:20 +0100

    Update PlayerController.js

--- client/js/player/PlayerController.js
+++ client/js/player/PlayerController.js
@@ -1,1 +1,36 @@
+import PlayerConfig from "./PlayerConfig";
 
+export default class PlayerController {
+
+    constructor() {
+
+        this.position = {
+            x: 0,
+            y: PlayerConfig.height,
+            z: 0
+        };
+
+        this.velocity = {
+            x: 0,
+            y: 0,
+            z: 0
+        };
+
+        this.rotation = {
+            yaw: 0,
+            pitch: 0
+        };
+
+        this.isGrounded = true;
+
+        this.isRunning = false;
+
+    }
+
+    update(delta) {
+
+        // سنضيف الحركة هنا في الجزء الثاني
+
+    }
+
+}
