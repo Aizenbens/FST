ee8c9dab47b69c7d61b5a4f9832c03b7cd7ce18c
Aizenbens <sheninozen@gmail.com>
2026-07-25 06:34:10 -0700

    Update Movement.js

--- client/js/player/Movement.js
+++ client/js/player/Movement.js
@@ -1,3 +1,4 @@
+import { isKeyPressed } from "../input/Input";
 
 export default class Movement {
 
@@ -5,9 +6,25 @@ export default class Movement {
         this.speed = 6;
         this.sprintSpeed = 10;
         this.jumpForce = 8;
+
+        this.direction = {
+            x: 0,
+            z: 0
+        };
     }
 
     update(delta) {
+
+        this.direction.x = 0;
+        this.direction.z = 0;
+
+        if (isKeyPressed("KeyW")) this.direction.z -= 1;
+        if (isKeyPressed("KeyS")) this.direction.z += 1;
+
+        if (isKeyPressed("KeyA")) this.direction.x -= 1;
+        if (isKeyPressed("KeyD")) this.direction.x += 1;
+
+        // الحركة الحقيقية سنضيفها في الخطوة القادمة
 
     }
 
