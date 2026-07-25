9b6a3d8bd0adfd9afd80be3d2b59368a47da282f
Aizenbens <sheninozen@gmail.com>
2026-07-25 06:51:46 -0700

    Update Ground.jsx

--- client/js/world/Ground.jsx
+++ client/js/world/Ground.jsx
@@ -3,9 +3,15 @@ export default function Ground() {
         <mesh
             rotation={[-Math.PI / 2, 0, 0]}
             receiveShadow
+            position={[0, 0, 0]}
         >
-            <planeGeometry args={[200, 200]} />
-            <meshStandardMaterial color="#4CAF50" />
+            <planeGeometry args={[200, 200, 50, 50]} />
+
+            <meshStandardMaterial
+                color="#5d8c42"
+                roughness={1}
+                metalness={0}
+            />
         </mesh>
     );
 }
