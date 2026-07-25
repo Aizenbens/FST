5bf20c2142a767fbb04b1cd67b76243db73dacd5
Aizenbens <sheninozen@gmail.com>
2026-07-25 13:08:30 +0100

    Update World.jsx

--- client/js/world/World.jsx
+++ client/js/world/World.jsx
@@ -1,8 +1,10 @@
 import Ground from "./Ground";
+import Sky from "./Sky";
 
 export default function World() {
     return (
         <>
+            <Sky />
             <Ground />
         </>
     );
