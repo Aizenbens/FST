75224e808e480a497c69a758b701b45266ea3592
Aizenbens <sheninozen@gmail.com>
2026-07-25 12:47:18 +0100

    Update Player.jsx

--- client/js/Player.jsx
+++ client/js/Player.jsx
@@ -1,8 +1,28 @@
+import Ecctrl from "ecctrl";
+import { CapsuleCollider } from "@react-three/rapier";
+
 export default function Player() {
-    return (
-        <mesh position={[0, 1, 0]} castShadow>
-            <capsuleGeometry args={[0.4, 1]} />
-            <meshStandardMaterial color="#4da6ff" />
-        </mesh>
-    );
+  return (
+    <Ecctrl
+      animated={false}
+      capsuleHalfHeight={0.35}
+      capsuleRadius={0.35}
+      camCollision
+      camInitDis={0}
+      camMinDis={0}
+      camMaxDis={0}
+      camFollowMult={100}
+      turnSpeed={100}
+      maxVelLimit={8}
+      jumpVel={6}
+      sprintMult={1.7}
+    >
+      <CapsuleCollider args={[0.35, 0.35]} />
+
+      <mesh castShadow>
+        <capsuleGeometry args={[0.35, 1]} />
+        <meshStandardMaterial color="#4DA6FF" />
+      </mesh>
+    </Ecctrl>
+  );
 }
