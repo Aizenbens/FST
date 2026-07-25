dc75b04683facd5d3351d5a1b7ae7a29c285a43a
Aizenbens <sheninozen@gmail.com>
2026-07-25 12:44:43 +0100

    Update Player.jsx

--- client/js/Player.jsx
+++ client/js/Player.jsx
@@ -1,1 +1,8 @@
-
+export default function Player() {
+    return (
+        <mesh position={[0, 1, 0]} castShadow>
+            <capsuleGeometry args={[0.4, 1]} />
+            <meshStandardMaterial color="#4da6ff" />
+        </mesh>
+    );
+}
