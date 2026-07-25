c82bded7b51684fc1e9af0380c7e15ab5dc9a736
Aizenbens <sheninozen@gmail.com>
2026-07-25 13:23:50 +0100

    Update Environment.jsx

--- client/js/scene/Environment.jsx
+++ client/js/scene/Environment.jsx
@@ -1,1 +1,17 @@
+import React from "react";
 
+export default function Environment() {
+    return (
+        <>
+            <color
+                attach="background"
+                args={["#87CEEB"]}
+            />
+
+            <fog
+                attach="fog"
+                args={["#87CEEB", 50, 250]}
+            />
+        </>
+    );
+}
