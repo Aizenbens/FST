51c3052db347057bafe71c7284333ae4b5dc115a
Aizenbens <sheninozen@gmail.com>
2026-07-25 17:42:06 +0100

    Update PlayerConfig.js

--- client/js/player/PlayerConfig.js
+++ client/js/player/PlayerConfig.js
@@ -1,1 +1,23 @@
+const PlayerConfig = {
 
+    height: 1.7,
+
+    radius: 0.35,
+
+    speed: 6,
+
+    sprintSpeed: 10,
+
+    jumpForce: 8,
+
+    gravity: 9.81,
+
+    mouseSensitivity: 0.0025,
+
+    maxLookUp: Math.PI / 2 - 0.05,
+
+    maxLookDown: -(Math.PI / 2 - 0.05)
+
+};
+
+export default PlayerConfig;
