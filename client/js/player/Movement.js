c22f62e38cb7a3f877922a051d85ca7bcdba776c
Aizenbens <sheninozen@gmail.com>
2026-07-25 06:29:41 -0700

    Update Movement.js

--- client/js/player/Movement.js
+++ client/js/player/Movement.js
@@ -1,1 +1,14 @@
 
+export default class Movement {
+
+    constructor() {
+        this.speed = 6;
+        this.sprintSpeed = 10;
+        this.jumpForce = 8;
+    }
+
+    update(delta) {
+
+    }
+
+}
