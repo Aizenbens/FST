86a1833d4146e229ed4b9448ac641ed649c110fb
Aizenbens <sheninozen@gmail.com>
2026-07-25 06:34:40 -0700

    Update CameraController.jsx

--- client/js/player/CameraController.jsx
+++ client/js/player/CameraController.jsx
@@ -1,4 +1,27 @@
+import { useThree } from "@react-three/fiber";
+import { useEffect } from "react";
+
 export default function CameraController() {
+
+    const { camera, gl } = useThree();
+
+    useEffect(() => {
+
+        camera.position.set(0, 1.7, 5);
+
+        const canvas = gl.domElement;
+
+        function lockPointer() {
+            canvas.requestPointerLock();
+        }
+
+        canvas.addEventListener("click", lockPointer);
+
+        return () => {
+            canvas.removeEventListener("click", lockPointer);
+        };
+
+    }, [camera, gl]);
 
     return null;
 
