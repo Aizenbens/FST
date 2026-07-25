957d609dd6e5cec3f72b93b99e145e40af0cc250
Aizenbens <sheninozen@gmail.com>
2026-07-25 06:30:39 -0700

    Update Player.jsx

--- client/js/player/Player.jsx
+++ client/js/player/Player.jsx
@@ -1,9 +1,29 @@
-export default function Player() {
-    return (
-        <mesh position={[0, 1, 0]} castShadow>
-            <capsuleGeometry args={[0.35, 1, 8, 16]} />
+import CameraController from "./CameraController";
+import Movement from "./Movement";
 
-            <meshStandardMaterial color="#ff8c00" />
-        </mesh>
+const movement = new Movement();
+
+export default function Player() {
+
+    movement.update();
+
+    return (
+        <>
+            <CameraController />
+
+            <mesh
+                castShadow
+                position={[0, 1, 0]}
+            >
+                <capsuleGeometry
+                    args={[0.35, 1, 8, 16]}
+                />
+
+                <meshStandardMaterial
+                    color="#ff8c00"
+                />
+            </mesh>
+        </>
     );
+
 }
